b1bd59a04f7ee62a7d3f960425d8682b74b74906
Entrivax <entrivax@gmail.com>
2020-01-20 13:41:31 +0100

    Add restore ability to the original Arduino sketch

--- AmiiBombuino/AmiiBombuino.ino
+++ AmiiBombuino/AmiiBombuino.ino
@@ -21,6 +21,7 @@ void setup()
   SCmd.addCommand("/NTAG_HALT", NTAG_Halt);
   SCmd.addCommand("/READ_AMIIBO", Read_Amiibo);
   SCmd.addCommand("/WRITE_AMIIBO", Write_Amiibo);
+  SCmd.addCommand("/RESTORE_AMIIBO", Restore_Amiibo);
 }
 
 void loop()
@@ -95,6 +96,122 @@ void Read_Amiibo()
   Serial.print(End_of_Message);
 }
 
+/**
+ * Authenticate with a NTAG216.
+ * 
+ * Only for NTAG216. First implemented by Gargantuanman.
+ * 
+ * @param[in]   passWord   password.
+ * @param[in]   pACK       result success???.
+ * @return STATUS_OK on success, STATUS_??? otherwise.
+ */
+MFRC522::StatusCode PCD_NTAG216_AUTH(byte* passWord, byte pACK[]) //Authenticate with 32bit password
+{
+  // TODO: Fix cmdBuffer length and rxlength. They really should match.
+  //       (Better still, rxlength should not even be necessary.)
+
+  MFRC522::StatusCode result;
+  byte        cmdBuffer[18]; // We need room for 16 bytes data and 2 bytes CRC_A.
+  
+  cmdBuffer[0] = 0x1B; //Comando de autentificacion
+  
+  for (byte i = 0; i<4; i++)
+    cmdBuffer[i+1] = passWord[i];
+  
+  result = mfrc522.PCD_CalculateCRC(cmdBuffer, 5, &cmdBuffer[5]);
+  
+  if (result!=MFRC522::STATUS_OK) {
+    return result;
+  }
+  
+  // Transceive the data, store the reply in cmdBuffer[]
+  byte waitIRq    = 0x30; // RxIRq and IdleIRq
+//  byte cmdBufferSize  = sizeof(cmdBuffer);
+  byte validBits    = 0;
+  byte rxlength   = 5;
+  result = mfrc522.PCD_CommunicateWithPICC(MFRC522::PCD_Transceive, waitIRq, cmdBuffer, 7, cmdBuffer, &rxlength, &validBits);
+  
+  pACK[0] = cmdBuffer[0];
+  pACK[1] = cmdBuffer[1];
+  
+  if (result!=MFRC522::STATUS_OK) {
+    return result;
+  }
+  
+  return MFRC522::STATUS_OK;
+} // End PCD_NTAG216_AUTH()
+
+byte authenticate()
+{
+    int uid[7];
+    for (byte i = 0; i < 7; i++)
+    {
+        uid[i] = mfrc522.uid.uidByte[i];
+    }
+    byte password[4];
+    password[0] = ((byte)(0xFF & (0xAA ^ (uid[1] ^ uid[3]))));
+    password[1] = ((byte)(0xFF & (0x55 ^ (uid[2] ^ uid[4]))));
+    password[2] = ((byte)(0xFF & (0xAA ^ (uid[3] ^ uid[5]))));
+    password[3] = ((byte)(0xFF & (0x55 ^ (uid[4] ^ uid[6]))));
+    byte resp[2];
+    MFRC522::StatusCode status;
+    status = (MFRC522::StatusCode)PCD_NTAG216_AUTH(password, resp);
+    if (status != MFRC522::STATUS_OK)
+    {
+        Serial.print(Begin_of_Message);
+        Serial.print("/ERROR Auth: ");
+        Serial.print(mfrc522.GetStatusCodeName(status));
+        Serial.print(End_of_Message);
+        return 1;
+    }
+    return 0;
+}
+
+void Restore_Amiibo() {
+    byte buffer[0x21C];
+
+    Serial.print(Begin_of_Message);
+    Serial.print("/WAIT");
+    Serial.print(End_of_Message);
+
+    while (Serial.available() == 0)
+    {
+    }
+
+    Serial.readBytes(buffer, 0x21C);
+
+    int authResult = authenticate();
+    if (authResult == 1) {
+        return;
+    }
+
+    MFRC522::StatusCode status;
+
+    // Write Data
+    for (byte page = 4; page < 135; page++)
+    {
+        if ((page >= 0xD && page < 0x20) || (page >= 130 && page < 133)) {
+            continue;
+        }
+        status = (MFRC522::StatusCode)mfrc522.MIFARE_Ultralight_Write(page, buffer + (page * 4), 4);
+        if (status != MFRC522::STATUS_OK)
+        {
+            Serial.print(Begin_of_Message);
+            Serial.print("/ERROR Data: ");
+            Serial.print(mfrc522.GetStatusCodeName(status));
+            Serial.print(End_of_Message);
+            break;
+        }
+    }
+
+    if (status == MFRC522::STATUS_OK)
+    {
+        Serial.print(Begin_of_Message);
+        Serial.print("/END_WRITE");
+        Serial.print(End_of_Message);
+    }
+}
+
 void Write_Amiibo()
 {
   char *arg;
@@ -164,4 +281,3 @@ void Write_Amiibo()
     Serial.print(End_of_Message);
   }
 }
-
